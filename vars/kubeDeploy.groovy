6b9b5fe8ef226eb1fac226d37d3ebc009dc51750
prasad440 <prasadkovvuri440@gmail.com>
2023-11-26 19:20:21 +0530

    kube deploy and delete

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,5 +1,5 @@
 def call() {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
-        sh "kubectl apply -f deployment.yml"
+        sh "/usr/local/bin/kubectl apply -f deployment.yml"
     }
 }
